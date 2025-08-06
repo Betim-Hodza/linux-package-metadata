68613ee5b2fdda444929c89910a6d5f8852087ba
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2025-08-06 19:55:15 +0000

    update it works, just takes a looong time

--- ubuntu/ubuntu_packages.sh
+++ ubuntu/ubuntu_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the base URL
-base_url="https://mirrors.kernel.org/ubuntu/pool/universe"
+UBUNTU_COMPONENTS=("main" "restricted" "universe" "multiverse")
 
 # Set the temporary directory
 TEMP_DIR="temp"
@@ -10,47 +10,56 @@ mkdir -p "$TEMP_DIR"
 OUTPUT_DIR="packages"
 mkdir -p "$OUTPUT_DIR"
 
-# Loop through all the folders in the universe pool
-for folder in $(curl -s  -L "$base_url" | grep -oE '<a href="[^"]+">[^<]+</a>' | sed -r 's/<a href="([^"]+)">[^<]+<\/a>/\1/'); do
-  # Loop through all the subfolders in the current folder
-  for subfolder in $(curl -s -L "$base_url/$folder" | grep -oE '<a href="[^"]+">[^<]+</a>' | sed -r 's/<a href="([^"]+)">[^<]+<\/a>/\1/'); do
-    # Download all the packages in the current subfolder
-    for PACKAGE in $(curl -s  -L "$base_url/$folder/$subfolder" | grep -oE '<a href="[^"]+\.deb">[^<]+\.deb</a>' | sed -r 's/<a href="([^"]+\.deb)">[^<]+\.deb<\/a>/\1/'); do
-      # Download the package
-      PACKAGE_URL="$base_url/$folder/$subfolder/$PACKAGE"
-      PACKAGE_FILE="$TEMP_DIR/$PACKAGE"
-      wget -q -O "$PACKAGE_FILE" "$PACKAGE_URL"
+for component in "${UBUNTU_COMPONENTS[@]}"; do
+  base_url="https://mirrors.kernel.org/ubuntu/pool/${component}"
+  # Loop through all the folders in the universe pool
+  for folder in $(curl -s  -L "$base_url" | grep -oE '<a href="[^"]+">[^<]+</a>' | sed -r 's/<a href="([^"]+)">[^<]+<\/a>/\1/'); do
+    # Loop through all the subfolders in the current folder
+    for subfolder in $(curl -s -L "$base_url/$folder" | grep -oE '<a href="[^"]+">[^<]+</a>' | sed -r 's/<a href="([^"]+)">[^<]+<\/a>/\1/'); do
+      # Download all the packages in the current subfolder
+      PACKAGES=$(curl -s  -L "$base_url/$folder/$subfolder" | grep -oE '<a href="[^"]+\.deb">[^<]+\.deb</a>' | sed -r 's/<a href="([^"]+\.deb)">[^<]+\.deb<\/a>/\1/')
+      PACKAGES=($PACKAGES)
 
-      
+      # Split the list of packages into batches of 10
+      for ((i=0; i<${#PACKAGES[@]}; i+=10)); do
+        BATCH=("${PACKAGES[@]:$i:10}")
 
-      # get name and ver and extract deb file
-      PACKAGE_NAME=$(echo "$PACKAGE" | cut -d '_' -f 1)
-      PACKAGE_VERSION=$(echo "$PACKAGE" | cut -d '_' -f 2)
-      PACKAGE_SHA256=$(sha256sum "$PACKAGE_FILE")
+        # Process the batch of packages in the background
+        (
+          for PACKAGE in "${BATCH[@]}"; do
+            PACKAGE_URL="$base_url/$folder/$subfolder/$PACKAGE"
+            PACKAGE_FILE="$TEMP_DIR/$PACKAGE"
+            wget -q -O "$PACKAGE_FILE" "$PACKAGE_URL"
 
-      # Save package metdata
-      echo "$PACKAGE_NAME,$PACKAGE_VERSION,$PACKAGE_SHA256,$PACKAGE_URL" >> "$OUTPUT_DIR/packages.csv"
-    
-      PACKAGE_DIR="$TEMP_DIR/$PACKAGE_NAME-$PACKAGE_VERSION"
-      mkdir -p "$PACKAGE_DIR"
-      
-      dpkg-deb -x "$PACKAGE_FILE" "$PACKAGE_DIR"
+            PACKAGE_NAME=$(echo "$PACKAGE" | cut -d '_' -f 1)
+            PACKAGE_VERSION=$(echo "$PACKAGE" | cut -d '_' -f 2)
 
-      # Calculate the SHA-256 sum for all files in the package
-      SHA256SUMS=()
-      FILE_PATHS=()
-      for FILE in $(find "$PACKAGE_DIR" -type f); do
-        echo "$PACKAGE_NAME,$PACKAGE_VERSION,$FILE,$(sha256sum "$FILE" | cut -d ' ' -f 1),$PACKAGE_URL" >> "$OUTPUT_DIR/packages.csv"
-        # SHA256SUM=$(sha256sum "$FILE" | cut -d ' ' -f 1)
-        # SHA256SUMS+=("$SHA256SUM")
-        # FILE_PATHS+=("$FILE")
+            PACKAGE_DIR="$TEMP_DIR/$PACKAGE_NAME-$PACKAGE_VERSION"
+            mkdir -p "$PACKAGE_DIR"
+
+            dpkg-deb -x "$PACKAGE_FILE" "$PACKAGE_DIR"
+
+            # Calculate the SHA-256 sum of the package file
+            PACKAGE_SHA256SUM=$(sha256sum "$PACKAGE_FILE" | cut -d ' ' -f 1)
+
+            # Save the package metadata
+            echo "$PACKAGE_NAME,$PACKAGE_VERSION,$PACKAGE_SHA256SUM,$PACKAGE_URL" >> "$OUTPUT_DIR/packages.csv"
+
+            # Loop through all the files in the package
+            for FILE in $(find "$PACKAGE_DIR" -type f); do
+              # Calculate the SHA-256 sum of the file
+              FILE_SHA256SUM=$(sha256sum "$FILE" | cut -d ' ' -f 1)
+
+              # Save the file metadata
+              echo "$PACKAGE_NAME,$PACKAGE_VERSION,$FILE_SHA256SUM,$FILE,$PACKAGE_URL" >> "$OUTPUT_DIR/files.csv"
+            done
+
+            rm "$PACKAGE_FILE"
+            rm -rf "$PACKAGE_DIR"
+          done
+        ) &
       done
-
-    
-
-      rm "$PACKAGE_FILE"
-      rm -rf "$PACKAGE_DIR"
-
+      wait
     done
   done
 done
